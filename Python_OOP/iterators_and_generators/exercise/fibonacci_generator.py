ba2f67004554123f70c0d345a6644f659bb26d4c
ivanoff-ivan <aenlordish@gmail.com>
2020-09-27 15:16:34 +0300

    python oop

---
+++ Python_OOP/iterators_and_generators/exercise/fibonacci_generator.py
@@ -0,0 +1,13 @@
+def fibonacci():
+    first_num = 0
+    second_num = 1
+    while True:
+        yield first_num
+        second_num += first_num
+        yield second_num
+        first_num += second_num
+
+
+generator = fibonacci()
+for i in range(5):
+    print(next(generator))
